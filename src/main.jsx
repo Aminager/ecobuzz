26be2874394a828e2281f803515d462a48e67919
Amin Alian <alianamin00@gmail.com>
2023-11-21 13:39:44 +0100

    First commit

---
+++ src/main.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import Home from "./pages/Home";
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(<Route path="/" element={<Home />}></Route>)
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
---
+++ src/pages/Home.jsx
@@ -0,0 +1,3 @@
+export default function Home() {
+  return <div className="bg-lime-300 w-screen h-screen"></div>;
+}

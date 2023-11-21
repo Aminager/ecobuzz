4c6e19560f7364786801da64fa2c3e0ffc8a9a50
Amin Alian <alianamin00@gmail.com>
2023-11-21 14:03:14 +0100

    Added NavBar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,10 @@
+import NavBar from "../components/NavBar";
+
 export default function Home() {
-  return <div className="bg-lime-300 w-screen h-screen"></div>;
+  return (
+    <div className="block bg-lime-300 w-screen h-screen text-center">
+      <h1 className="text-4xl text-gray-800 font-bold p-10">EcoBuzz</h1>
+      <NavBar />
+    </div>
+  );
 }

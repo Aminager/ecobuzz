2a36963d64be6ea6d98da0ec5a0d0899e848b8a8
Amin Alian <alianamin00@gmail.com>
2023-11-28 14:55:36 +0100

    EZ LANDING

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,20 @@
 import "../bubbles.css";
 import EnvironmentalImpact from "../components/EnvironmentalImpact";
 import WhoAreWe from "../components/WhoAreWe";
-import { LEFT_SIDE_TEXT } from "../text";
 
 export default function Home() {
   function createBubble() {
     const section = document.querySelector("Section");
     const createElement = document.createElement("span");
-    var size = Math.random() * 7;
+    var size = Math.random() * 2;
 
     createElement.style.animation = "animation 6s linear infinite";
     createElement.style.width = 2 + size + "vw";
     createElement.style.height = 2 + size + "vw";
-    createElement.style.left = Math.random() * (innerWidth * 0.6) + "px";
+    // börja på 750 -> 1150
+    createElement.style.left = 750 + Math.random() * 400 + "px";
+    createElement.style.top = 400 + Math.random() * 200 + "px";
+
     section.appendChild(createElement);
 
     setTimeout(() => {
@@ -20,25 +22,11 @@ export default function Home() {
     }, 10000);
   }
 
-  setInterval(createBubble, 3000);
+  setInterval(createBubble, 300);
 
   return (
-    <div className="flex flex-col justify-center items-center bg-lime-100 text-center h-screen overflow-hidden">
-      <div>
-        <h1 className="text-4xl text-gray-800 font-bold p-5">EcoBuzz</h1>
-      </div>
-      <div className="border-lime-300 border-y w-5/6" />
+    <div className="flex flex-col justify-center items-center bg-[url('src/img/ECOBUZZLANDING.png')] bg-cover text-center h-screen overflow-hidden">
       <section />
-      <div className="flex flex-row justify-evenly items-center w-full h-[85%]">
-        <div className="w-2/5 p-2">
-          <EnvironmentalImpact />
-        </div>
-
-        <div className="border-lime-300 border-x h-5/6" />
-        <div className="w-3/5 p-2">
-          <WhoAreWe />
-        </div>
-      </div>
     </div>
   );
 }
